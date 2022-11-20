b4784f925647ed01901256cd61f31e7c234f0591
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-11-20 18:17:29 -0300

    Iniciando formulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,7 @@
 import styles from './Post.module.css';
 
-export function Post(){
-  return(
+export function Post() {
+  return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
@@ -16,12 +16,24 @@ export function Post(){
       </header>
 
       <div className={styles.content}>
-          <p>Fala pessoal! </p>
-          <p>Acabei de subir mais um projeto no meu git!</p>
-          <p><a href="">pedrocmoreira/igniteFeed</a></p>
-          <a href="">#teste</a>{' '}
-          <a href="">#estudo</a>
+        <p>Fala pessoal! </p>
+        <p>Acabei de subir mais um projeto no meu git!</p>
+        <p><a href="">pedrocmoreira/igniteFeed</a></p>
+        <a href="">#teste</a>{' '}
+        <a href="">#estudo</a>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Deixe um comentário'
+        />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
